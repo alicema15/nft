9c38bc39a18a107a862f024abf810a203ebc98de
Alice Ma <alicema@berkeley.edu>
2021-10-13 14:43:57 -0400

    FEATURE: generate svg on chain

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,16 +1,16 @@
 const main = async () => {
-    const nftContractFactory = await hre.ethers.getContractFactory('MyEpicNFT');
+    const nftContractFactory = await hre.ethers.getContractFactory('NFT');
     const nftContract = await nftContractFactory.deploy();
     await nftContract.deployed();
     console.log("Contract deployed to:", nftContract.address);
   
     // Call the function.
-    let txn = await nftContract.makeAnEpicNFT()
+    let txn = await nftContract.mintNFT()
     // Wait for it to be mined.
     await txn.wait()
     console.log("Minted NFT #1")
   
-    txn = await nftContract.makeAnEpicNFT()
+    txn = await nftContract.mintNFT()
     // Wait for it to be mined.
     await txn.wait()
     console.log("Minted NFT #2")
